34c3f18ecf7b25698b5d7fe7cf4b910a03abc048
迷渡 <justjavac@gmail.com>
2020-09-05 23:57:59 +0800

    upgrade std@0.68.0 (#1)

--- mod_test.ts
+++ mod_test.ts
@@ -1,7 +1,7 @@
 import {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.63.0/testing/asserts.ts";
+} from "https://deno.land/std@0.68.0/testing/asserts.ts";
 
 import vendors from "./mod.ts";
 
