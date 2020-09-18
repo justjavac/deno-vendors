950e2cfead2ea9e49d7e4aab351be536cece1f54
Carter Snook <cartersnook04@gmail.com>
2020-09-17 19:08:14 -0500

    Add `isVendor()`

---
+++ is_vendor.ts
@@ -0,0 +1,12 @@
+import type { Vendor } from "./types/vendor.ts";
+import { vendors } from "./vendors.ts";
+
+/**
+ * Returns whether a given `vendor` string is an official vendor.
+ *
+ * @param vendor the given vendor string
+ * @returns boolean for if the `vendor` argument is in the `vendors` list
+ */
+export function isVendor(vendor: string) {
+  return vendors.includes(<Vendor>vendor);
+}
